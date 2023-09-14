0b3ea049e95f4d3d56783147ae88770046521295
최진우 <jk62362@mensakorea.org>
2023-09-15 07:50:22 +0900

    added 'running horse' gif

--- app/src/main/java/com/jinwoo/friendship/MainActivity.kt
+++ app/src/main/java/com/jinwoo/friendship/MainActivity.kt
@@ -4,7 +4,9 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.ImageButton
+import android.widget.ImageView
 import android.widget.TextView
+import com.bumptech.glide.Glide
 
 class MainActivity : AppCompatActivity() {
 
@@ -31,14 +33,34 @@ class MainActivity : AppCompatActivity() {
         val tv_meterFee: TextView = findViewById(R.id.tv_meterFee);
         val tv_meterWon: TextView = findViewById(R.id.tv_meterWon);
         val imgbtn_meterMain: ImageButton = findViewById(R.id.imgbtn_meterMain);
+        val iv_meterRunningHorse: ImageView = findViewById(R.id.iv_meterRunningHorse);
 
         imgbtn_meterMain.setOnClickListener {
             start();
         }
+
+        Glide.with(this)
+            .load(R.drawable.running_horse).override(500,300)
+            .skipMemoryCache(true)
+            .into(iv_meterRunningHorse)
     }
 
     fun oil() {
         setContentView(R.layout.activity_oil);
+        val tv_oilFee: TextView = findViewById(R.id.tv_oilFee);
+        val tv_oilWon: TextView = findViewById(R.id.tv_oilWon);
+        val imgbtn_oilMain: ImageButton = findViewById(R.id.imgbtn_oilMain);
+        val iv_oilRunningHorse: ImageView = findViewById(R.id.iv_oilRunningHorse);
+
+        imgbtn_oilMain.setOnClickListener {
+            start();
+        }
+
+
+        Glide.with(this)
+            .load(R.drawable.running_horse).override(500,300)
+            .skipMemoryCache(true)
+            .into(iv_oilRunningHorse)
     }
 
 
